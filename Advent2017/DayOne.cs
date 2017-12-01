36e4c24bcc098e5af80cb595cd1e8251938654c7
Jack Burgess <jjb@redwoodtech.com>
2017-12-01 09:59:58 +0000

    Day 1 Solution.

---
+++ Advent2017/DayOne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2017
+{
+    class DayOne
+    { 
+        public static int Solve(string input, int offset)
+        {
+            int res = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == input[(i + offset) % input.Length])
+                {
+                    res += (input[i] - '0');
+                }
+            }
+            
+            return res;
+        }
+    }
+}
