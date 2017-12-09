755c01bed358f5a9d7577ae12e46fe3af76b4926
Jack Burgess <jackjburgess@gmail.com>
2017-12-09 15:44:42 +0000

    Day 9 complete. comment out previous days so the input files arent needed.

---
+++ Advent2017/DayNine.cs
@@ -0,0 +1,115 @@
+﻿using System.Linq;
+
+namespace Advent2017
+{
+    class DayNine
+    {
+        public static int SolvePartOne(string input)
+        {
+            var res = 0;
+
+            var currentDepth = 0;
+            var negate = false;
+            var garbage = false;
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(!garbage)
+                {
+                    switch(input[i])
+                    {
+                        case '{':
+                            currentDepth++;
+                            break;
+                        case '}':
+                            res += currentDepth;
+                            currentDepth--;
+                            break;
+                        case '<':
+                            garbage = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    if (!negate)
+                    {
+                        if (input[i] == '>')
+                        {
+                            garbage = false;
+                        }
+                        else if (input[i] == '!')
+                        {
+                            negate = true;
+                        }
+                    }
+                    else
+                    {
+                        negate = false;
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        public static int SolvePartTwo(string input)
+        {
+            var res = 0;
+
+            var currentDepth = 0;
+            var negate = false;
+            var garbage = false;
+            var garbageCount = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!garbage)
+                {
+                    switch (input[i])
+                    {
+                        case '{':
+                            currentDepth++;
+                            break;
+                        case '}':
+                            res += currentDepth;
+                            currentDepth--;
+                            break;
+                        case '<':
+                            garbage = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    if (!negate)
+                    {
+                        if (input[i] == '>')
+                        {
+                            garbage = false;
+                        }
+                        else if (input[i] == '!')
+                        {
+                            negate = true;
+                        }
+                        else
+                        {
+                            garbageCount++;
+                        }
+                    }
+                    else
+                    {
+                        negate = false;
+                    }
+                }
+            }
+
+            return garbageCount;
+        }
+
+    }
+}
